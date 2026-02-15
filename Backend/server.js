0502dd667355dcf70d4492bf6117342ad15bb98e
itssoumodip <soumodipddas2005@gmail.com>
2026-02-15 13:55:54 +0530

    initialize Node.js backend with Express and Socket.IO for real-time chat functionality.

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,22 @@
 import { createServer } from 'node:http';
 import express from 'express';
 import { Server } from "socket.io";
-import e from 'express';
-import env from 'dotenv';
+import dotenv from 'dotenv';
 
-const PORT = env.PORT;
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('<h1>Server is running</h1>');
 });
 
 server.listen(PORT, () => {
